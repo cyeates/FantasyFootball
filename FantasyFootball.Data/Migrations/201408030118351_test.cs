f32f9ac00e4204e1e4e8ec731a3f73dfe5508ecd
Chad Yeates <chad.yeates@gmail.com>
2014-08-02 21:00:50 -0500

    Revert "removing test migration"
    This reverts commit ffc8e2884e95da3da744bde0530e86c330d6804a.

---
+++ FantasyFootball.Data/Migrations/201408030118351_test.cs
@@ -0,0 +1,16 @@
+namespace FantasyFootball.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class test : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
