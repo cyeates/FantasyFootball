ed1492caab7704a221b7641c0af4c062ebc32181
Chad Yeates <chad.yeates@gmail.com>
2014-08-05 17:40:35 -0500

    bundle cleanup

--- FantasyFootball/App_Start/BundleConfig.cs
+++ FantasyFootball/App_Start/BundleConfig.cs
@@ -17,14 +17,13 @@ namespace FantasyFootball
       bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
         "~/Scripts/angular.js",
         "~/Scripts/angular-resource.js",
-        "~/Scripts/kendo/2014.1.318/kendo.web.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.core.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.data.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.pager.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.tabstrip.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.sortable.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.grid.min.js",
-        //"~/Scripts/kendo/2014.1.318/kendo.validator.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.core.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.data.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.pager.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.tabstrip.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.sortable.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.grid.min.js",
+        "~/Scripts/kendo/2014.1.318/kendo.validator.min.js",
         "~/Scripts/angular-kendo.js",
         "~/Scripts/app/*.js"
         ));
